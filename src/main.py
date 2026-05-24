b1dfb16ea8536db2ea49807c03e390ae8d81350f
yusufyucetepe <yusufyucetepe06@gmail.com>
2026-05-24 19:10:16 +0300

    Added TextNode and TextType

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode(
+        "This is some anchor text",
+        TextType.LINK,
+        "https://www.boot.dev"
+    )
+
+    print(node)
+
+
+main()
