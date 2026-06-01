8c742098cd80a339798a2f811091e84e27ad79ec
yusufyucetepe <yusufyucetepe06@gmail.com>
2026-06-01 21:03:01 +0300

    Putting functions together in TextNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 from textnode import TextNode, TextType
 from textnode import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
-from textnode import split_nodes_image, split_nodes_link
+from textnode import split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -143,6 +143,38 @@ class TestTextNode(unittest.TestCase):
                 ),
             ],
             split_nodes_link([node]),
+        )
+
+    def test_text_to_textnodes(self):
+        text = (
+            "This is **text** with an _italic_ word and a "
+            "`code block` and an "
+            "![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) "
+            "and a [link](https://boot.dev)"
+        )
+
+        self.assertEqual(
+            text_to_textnodes(text),
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image",
+                    TextType.IMAGE,
+                    "https://i.imgur.com/fJRm4Vk.jpeg",
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode(
+                    "link",
+                    TextType.LINK,
+                    "https://boot.dev",
+                ),
+            ],
         )
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -168,3 +168,26 @@ def split_nodes_link(old_nodes):
             )
 
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(
+        nodes,
+        "**",
+        TextType.BOLD,
+    )
+    nodes = split_nodes_delimiter(
+        nodes,
+        "_",
+        TextType.ITALIC,
+    )
+    nodes = split_nodes_delimiter(
+        nodes,
+        "`",
+        TextType.CODE,
+    )
+
+    return nodes
