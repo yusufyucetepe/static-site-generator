6ec3406979c6c3f0f35ef143702fcbd6780c8350
yusufyucetepe <yusufyucetepe06@gmail.com>
2026-06-01 20:49:40 +0300

    Extract links added

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
+from textnode import TextNode, TextType
+from textnode import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -80,6 +81,36 @@ class TestTextNode(unittest.TestCase):
             TextNode("bold", TextType.BOLD),
             TextNode(" text", TextType.TEXT),
         ]
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")],
+            matches,
+        )
+
+    def test_extract_markdown_images_multiple(self):
+        matches = extract_markdown_images(
+            "![one](url1) and ![two](url2)"
+        )
+
+        self.assertListEqual(
+            [("one", "url1"), ("two", "url2")],
+            matches,
+        )
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "[Boot.dev](https://www.boot.dev)"
+        )
+
+        self.assertListEqual(
+            [("Boot.dev", "https://www.boot.dev")],
+            matches,
+        )
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 from htmlnode import LeafNode
 
@@ -76,3 +77,16 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(parts[i], text_type))
 
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(
+        r"!\[([^\[\]]*)\]\(([^\(\)]*)\)",
+        text,
+    )
+
+
+def extract_markdown_links(text):
+    return re.findall(
+        r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",
+        text,
+    )
