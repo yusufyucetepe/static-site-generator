ed82da33ae30c02cbc81e5901fd237e93719496e
yusufyucetepe <yusufyucetepe06@gmail.com>
2026-06-24 14:12:32 +0300

    Implemented recursive directory copy function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,26 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
 
-def main():
-    node = TextNode(
-        "This is some anchor text",
-        TextType.LINK,
-        "https://www.boot.dev"
-    )
+def copy_static(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
 
-    print(node)
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dst_path = os.path.join(dst, item)
+        if os.path.isfile(src_path):
+            print(f"Copying file: {src_path} -> {dst_path}")
+            shutil.copy(src_path, dst_path)
+        else:
+            copy_static(src_path, dst_path)
+
+
+def main():
+    copy_static("static", "public")
 
 
 main()
